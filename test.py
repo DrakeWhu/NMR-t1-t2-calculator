a7076e2a1867acb334da71ffffe578c10b753a70
Juan Rodríguez Pérez <juanrdg10@gmail.com>
2022-04-06 17:34:50 +0200

    Added raw data from NMR scanner, tested a way to read it

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
+import os
 import numpy as np
+from os import listdir
+from os.path import isfile, join
+import glob
+from pathlib import Path
 
-x = np.arange(0,1100,100)
-print(x)
+data_path = Path(r"C:/Users/juanr/Desktop/Universidad/Master/Practicas/programa calculo t1t2/NMR-t1-t2-calculator/rawdata/")
+file_list = [str(pp) for pp in data_path.glob("**/*.mat")]
+
+print(file_list)
+
+
